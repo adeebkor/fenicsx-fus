dbd25c8ec3bab93002424ebe5eac4da389050614
Adeeb Arif Kor <mabm4@cam.ac.uk>
2021-06-13 19:58:40 +0100

    Test animation

--- animate.py
+++ animate.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 # Load data file
-with open("test_1d_.npy", "rb") as file:
+data_fname = "test-model"
+with open("{}.npy".format(data_fname), "rb") as file:
     U = np.load(file)
 
 print("Number of snapshot:", U.shape)
 
-x0 = np.linspace(0, 0.1 * 4.5, 10000)
+# Set points
+x0 = np.linspace(0, 0.45, 10000)
 
 # Animate the figure
 fig = plt.figure(figsize=(16,8))
@@ -17,13 +19,13 @@ ims = []
 for i in range(U.shape[0]):
     p = plt.plot(x0, U[i, :], animated=True, color='k')
 
-    # plt.xlim([0.15, 0.2])
+    plt.xlim([0.07, 0.14])
 
     ims.append((p))
 
 ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
                                 repeat_delay=1000)
 
-filename = "anim1d.mp4"
+anim_fname = "test-model"
 
-ani.save(filename,  fps=2, extra_args=['-vcodec', 'libx264'])
+ani.save("{}.mp4".format(anim_fname), fps=2, extra_args=['-vcodec', 'libx264'])
