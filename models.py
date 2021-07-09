Fix sign

models.py
@@ -64,7 +64,7 @@ class Linear:
 
         # Update boundary condition
         with self.g.vector.localForm() as g_local:
-            g_local.set(-self.p0*self.w0/self.c0 * np.cos(self.w0 * t))
+            g_local.set(self.p0*self.w0/self.c0 * np.cos(self.w0 * t))
 
         # Update fields that depends on
         u.copy(result=self.u_n.vector)