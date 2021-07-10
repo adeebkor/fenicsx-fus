b45e9ba9f294eed7724dc4ad874d379696558b94
adeebkor <mabm4@cam.ac.uk>
2021-07-10 22:24:38 +0100

    Add LinearGLL model

--- models.py
+++ models.py
@@ -39,8 +39,8 @@ class Linear:
 
         # Build solver
         self.solver = PETSc.KSP().create(MPI.COMM_WORLD)
-        self.solver.setType(PETSc.KSP.Type.PREONLY)
-        self.solver.getPC().setType(PETSc.PC.Type.LU)
+        self.solver.setType("preonly")
+        self.solver.getPC().setType("lu")
         # self.solver.setType("cg")
         # self.solver.getPC().setType("jacobi")
         self.solver.setOperators(self.M)
@@ -80,6 +80,96 @@ class Linear:
                       mode=PETSc.ScatterMode.REVERSE)
 
         # Solve
+        self.solver.solve(b, result)
+
+        return result
+
+
+class LinearGLL:
+    """
+    Model that consider only diffraction (GLL)
+    """
+
+    def __init__(self, mesh, meshtag, k, c0, freq0, p0):
+        self.V = FunctionSpace(mesh, ("Lagrange", k))
+        self.u, self.v = TrialFunction(self.V), TestFunction(self.V)
+        self.g = Function(self.V)
+
+        # Tag boundary facets
+        ds = Measure('ds', subdomain_data=meshtag, domain=mesh)
+
+        # Physical parameters
+        self.c0 = c0
+        self.freq = freq0
+        self.w0 = 2 * np.pi * self.freq
+        self.p0 = p0
+
+        # Quadrature parameters
+        quad_params = {"quadrature_rule": "GLL",
+                       "quadrature_degree": k+1}
+
+        # Define variational formulation
+        self.a = inner(self.u, self.v)*dx(metadata=quad_params)
+        self.M = assemble_matrix(self.a)
+        self.M.assemble()
+
+        # Get diagonal
+        # self.m = PETSc.Vec().create(MPI.COMM_WORLD)
+        # lsize = self.M.getLocalSize()[0]
+        # gsize = self.M.getSize()[0]
+        # self.m.setSizes((lsize, gsize))
+        # self.m.setUp()
+        # self.M.getDiagonal(self.m)
+
+        self.u_n = Function(self.V)
+        self.v_n = Function(self.V)
+        self.L = self.c0**2*(
+            - inner(grad(self.u_n), grad(self.v))*dx(metadata=quad_params)
+            + inner(self.g, self.v)*ds(1, metadata=quad_params)
+            - 1/self.c0*inner(self.v_n, self.v)*ds(2, metadata=quad_params))
+
+        # Setup solver
+        self.solver = PETSc.KSP().create(MPI.COMM_WORLD)
+        self.solver.setType("preonly")
+        self.solver.getPC().setType("jacobi")
+        self.solver.setOperators(self.M)
+
+    def init(self):
+        """Return vectors with the initial condition"""
+        u, v = Function(self.V), Function(self.V)
+        with u.vector.localForm() as _u, v.vector.localForm() as _v:
+            _u.set(0.0)
+            _v.set(0.0)
+        return u, v
+
+    def f0(self, t: float, u: PETSc.Vec, v: PETSc.Vec, result: PETSc.Vec) \
+            -> PETSc.Vec:
+        """For du/dt = f0(t, u, v), return f0"""
+        return v.copy(result=result)
+
+    def f1(self, t: float, u: PETSc.Vec, v: PETSc.Vec, result: PETSc.Vec) \
+            -> PETSc.Vec:
+        """For dv/dt = f1(t, u, v), return f1"""
+
+        # Update boundary condition
+        with self.g.vector.localForm() as g_local:
+            g_local.set(self.p0*self.w0/self.c0 * np.cos(self.w0 * t))
+
+        # Update fields that depends on
+        u.copy(result=self.u_n.vector)
+        self.u_n.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT,
+                                    mode=PETSc.ScatterMode.FORWARD)
+        v.copy(result=self.v_n.vector)
+        self.v_n.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT,
+                                    mode=PETSc.ScatterMode.FORWARD)
+
+        # Assemble RHS
+        b = assemble_vector(self.L)
+        b.ghostUpdate(addv=PETSc.InsertMode.ADD,
+                      mode=PETSc.ScatterMode.REVERSE)
+
+        # Solve
+        # result.pointwiseDivide(b, self.m)
         self.solver.solve(b, result)
 
         return result
