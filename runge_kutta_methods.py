8bac596bbf2d6439236dd9696bcbbca94717f517
adeebkor <mabm4@cam.ac.uk>
2021-07-17 21:52:21 +0100

    Remove timing

--- runge_kutta_methods.py
+++ runge_kutta_methods.py
@@ -1,5 +1,3 @@
-import time
-
 import numpy as np
 from mpi4py import MPI
 from petsc4py import PETSc
@@ -83,10 +81,7 @@ def solve2(f0, f1, u, v, dt, num_steps, rk_order, filename=None):
         file.write_function(u, t=0.0)
 
     t = 0.0
-    ts = time.time()
     for step in range(num_steps):
-        # if step%100 == 1:
-        ts = time.time()
 
         # Store solution at start of time step
         u.vector.copy(result=u0)
@@ -116,11 +111,8 @@ def solve2(f0, f1, u, v, dt, num_steps, rk_order, filename=None):
         # Update time
         t += dt
 
-        te = time.time() - ts
-        PETSc.Sys.syncPrint("Steps:{}/{}, Solve time: {}".format(
-            step, num_steps, te))
-
-        if step % 100 == 0:
+        if step % 1000 == 0:
+            PETSc.Sys.syncPrint("Steps:{}/{}".format(step, num_steps))
             if filename is not None:
                 file.write_function(u, t=t)
 
