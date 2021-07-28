2bc8d7b06be9f29950eeb1b1692e5a94c704168e
Adeeb Arif Kor <mabm4@cam.ac.uk>
2021-07-28 13:49:53 +0100

    Add implementation to use SciPy ODE solver

--- models.py
+++ models.py
@@ -85,6 +85,95 @@ class Linear:
         return result
 
 
+class Linears:
+    """
+    Model that consider only diffraction
+    """
+
+    def __init__(self, mesh, meshtag, k, c0, freq0, p0):
+        self.V = FunctionSpace(mesh, ("Lagrange", k))
+        self.u, self.v = TrialFunction(self.V), TestFunction(self.V)
+        self.g = Function(self.V)
+
+        # Tag boundary facets
+        ds = Measure('ds', subdomain_data=meshtag, domain=mesh)
+
+        # Physical parameters
+        self.c0 = c0
+        self.freq = freq0
+        self.w0 = 2 * np.pi * self.freq
+        self.p0 = p0
+
+        # Define variational formulation
+        self.a = inner(self.u, self.v)*dx
+        self.M = assemble_matrix(self.a)
+        self.M.assemble()
+
+        self.u_n = Function(self.V)
+        self.v_n = Function(self.V)
+        self.L = self.c0**2*(-inner(grad(self.u_n), grad(self.v))*dx
+                             + inner(self.g, self.v)*ds(1)
+                             - 1/self.c0*inner(self.v_n, self.v)*ds(2))
+
+        # Build solver
+        self.solver = PETSc.KSP().create(MPI.COMM_WORLD)
+        self.solver.setType(PETSc.KSP.Type.PREONLY)
+        self.solver.getPC().setType(PETSc.PC.Type.LU)
+        # self.solver.setType("cg")
+        # self.solver.getPC().setType("jacobi")
+        self.solver.setOperators(self.M)
+
+        # Define helper vector
+        self.un = Function(self.V)
+        self.vn = Function(self.V)
+
+    def init(self):
+        """Return vectors with the initial condition"""
+        u, v = Function(self.V), Function(self.V)
+        with u.vector.localForm() as _u, v.vector.localForm() as _v:
+            _u.set(0.0)
+            _v.set(0.0)
+        return u, v
+
+    def f0(self, t: float, u: PETSc.Vec, v: PETSc.Vec, result: PETSc.Vec) \
+            -> PETSc.Vec:
+        """For du/dt = f0(t, u, v), return f0"""
+        return v.copy(result=result)
+
+    def f1(self, t: float, u: PETSc.Vec, v: PETSc.Vec, result: PETSc.Vec) \
+            -> PETSc.Vec:
+        """For dv/dt = f1(t, u, v), return f1"""
+
+        # Update boundary condition
+        with self.g.vector.localForm() as g_local:
+            g_local.set(self.p0*self.w0/self.c0 * np.cos(self.w0 * t))
+
+        # Update fields that depends on
+        u.copy(result=self.u_n.vector)
+        self.u_n.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT,
+                                    mode=PETSc.ScatterMode.FORWARD)
+        v.copy(result=self.v_n.vector)
+        self.v_n.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT,
+                                    mode=PETSc.ScatterMode.FORWARD)
+
+        # Assemble RHS
+        b = assemble_vector(self.L)
+        b.ghostUpdate(addv=PETSc.InsertMode.ADD,
+                      mode=PETSc.ScatterMode.REVERSE)
+
+        # Solve
+        self.solver.solve(b, result)
+
+        return result
+
+    def f(self, t, y):
+        self.un.x.array[:] = y[:y.shape[0]//2]
+        self.vn.x.array[:] = y[y.shape[0]//2:]
+        vdot = self.f1(t, self.un.vector, self.vn.vector, self.un.vector)
+
+        return np.concatenate([self.vn.x.array[:], vdot.array[:]])
+
+
 class LinearGLL:
     """
     Model that consider only diffraction (GLL)
@@ -241,6 +330,94 @@ class LinearGLLv:
 
         return result
 
+
+class LinearGLLvs:
+    """
+    Model that consider only diffraction (GLL)
+    """
+
+    def __init__(self, mesh, meshtag, k, c0, freq0, p0):
+        self.V = FunctionSpace(mesh, ("Lagrange", k))
+        self.u, self.v = Function(self.V), TestFunction(self.V)
+        self.g = Function(self.V)
+
+        # Tag boundary facets
+        ds = Measure('ds', subdomain_data=meshtag, domain=mesh)
+
+        # Physical parameters
+        self.c0 = c0
+        self.freq = freq0
+        self.w0 = 2 * np.pi * self.freq
+        self.p0 = p0
+
+        # Quadrature parameters
+        qd = {"2": 3, "3": 4, "4": 6, "5": 8}
+        quad_params = {"quadrature_rule": "GLL",
+                       "quadrature_degree": qd[str(k)]}
+
+        # Define variational formulation
+        self.u.x.array[:] = 1
+        self.a = inner(self.u, self.v)*dx(metadata=quad_params)
+        self.m = assemble_vector(self.a)
+
+        self.u_n = Function(self.V)
+        self.v_n = Function(self.V)
+        self.L = self.c0**2*(
+            - inner(grad(self.u_n), grad(self.v))*dx
+            + inner(self.g, self.v)*ds(1)
+            - 1/self.c0*inner(self.v_n, self.v)*ds(2))
+
+        # Define helper vector
+        self.un = Function(self.V)
+        self.vn = Function(self.V)
+
+    def init(self):
+        """Return vectors with the initial condition"""
+        u, v = Function(self.V), Function(self.V)
+        with u.vector.localForm() as _u, v.vector.localForm() as _v:
+            _u.set(0.0)
+            _v.set(0.0)
+        return u, v
+
+    def f0(self, t: float, u: PETSc.Vec, v: PETSc.Vec, result: PETSc.Vec) \
+            -> PETSc.Vec:
+        """For du/dt = f0(t, u, v), return f0"""
+        return v.copy(result=result)
+
+    def f1(self, t: float, u: PETSc.Vec, v: PETSc.Vec, result: PETSc.Vec) \
+            -> PETSc.Vec:
+        """For dv/dt = f1(t, u, v), return f1"""
+
+        # Update boundary condition
+        with self.g.vector.localForm() as g_local:
+            g_local.set(self.p0*self.w0/self.c0 * np.cos(self.w0 * t))
+
+        # Update fields that depends on
+        u.copy(result=self.u_n.vector)
+        self.u_n.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT,
+                                    mode=PETSc.ScatterMode.FORWARD)
+        v.copy(result=self.v_n.vector)
+        self.v_n.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT,
+                                    mode=PETSc.ScatterMode.FORWARD)
+
+        # Assemble RHS
+        b = assemble_vector(self.L)
+        b.ghostUpdate(addv=PETSc.InsertMode.ADD,
+                      mode=PETSc.ScatterMode.REVERSE)
+
+        # Solve
+        result.pointwiseDivide(b, self.m)
+
+        return result
+
+    def f(self, t, y):
+        self.un.x.array[:] = y[:y.shape[0]//2]
+        self.vn.x.array[:] = y[y.shape[0]//2:]
+        vdot = self.f1(t, self.un.vector, self.vn.vector, self.un.vector)
+
+        return np.concatenate([self.vn.x.array[:], vdot.array[:]])
+
+    
 
 class Lossy:
     """
