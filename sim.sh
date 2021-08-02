b818822ec8c7989ec4cde5ddca16afb1418f0990
Adeeb Arif Kor <mabm4@cam.ac.uk>
2021-08-02 14:25:43 +0100

    Add H1 error

--- sim.sh
+++ sim.sh
@@ -2,12 +2,12 @@
 
 for d in '2' '3' '4' '5'
     do
-    for e in '4' '8' '16' '32' '64'
+    for e in '8' '16' '32' '64'
 	    do
-        for t in '0.1' '0.01' '0.001' '0.0001'
+        for t in '0.001' '0.000001' '0.000000001' '0.000000000001'
             do
 				echo "Degree: $d, Element: $e, Tolerance: $t"
-    	        python3 linear_1d_gllv_direct.py $d $e $t
+    	        python3 linear_1d_gllv_scipy.py $d $e $t
     	    done
         done
 	done
