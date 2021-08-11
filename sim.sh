fab0d39d30c131d86f1d00640f009fa9d083274d
adeebkor <mabm4@cam.ac.uk>
2021-08-11 19:46:48 +0100

    Remove and refactor code

--- sim.sh
+++ sim.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for d in '2' '3' '4' '5'
+for d in '2' '3' 
     do
     for e in '8' '16' '32' '64'
 	    do
-        for t in '0.001' '0.000001' '0.000000001' '0.000000000001'
+        for t in '1e-3' '1e-6' '1e-9' '1e-12'
             do
 				echo "Degree: $d, Element: $e, Tolerance: $t"
-    	        python3 linear_1d_gllv_scipy.py $d $e $t
+    	        python3 linear_1d_scipy.py $d $e $t
     	    done
         done
 	done
