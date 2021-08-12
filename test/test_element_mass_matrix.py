de1bb6f0cd1e1a47db2646843a095be6c914ca6d
Adeeb Arif Kor <mabm4@cam.ac.uk>
2021-08-12 15:36:58 +0100

    Update codes (#5)
    * Add examples of ufl.action implementation
    
    * Update plots
    
    * ACompare cspeed up of different compiler options
    
    * Vector implementation of GLL mass matrix
    
    * Add GLLv models
    
    * Rename timing files
    
    * Update error data
    
    * Update plots
    
    * Remove file
    
    * Update plots
    
    * Change ufl.action implementation
    
    * Use implementation similar to vector
    
    * Update 2D plots
    
    * Update 3D data
    
    * Update plots
    
    * Update .gitignore
    
    * Initial commit
    
    * Remove plots
    
    * Initial commit
    
    * Change cell type
    
    * Remove line
    
    * Initial commit
    
    * Rename and add linear 3d problem
    
    * Organise files
    
    * Flake8 fix
    
    * Change data file type
    
    * Change solver
    
    * Remove timing
    
    * Add code for convergence study
    
    * Use dict to determine quad degree
    
    * Change definition of L2 error
    
    * Change degree list
    
    * Add linear 1D GLLv convergence study
    
    * Test adaptive RK
    
    * Add implementation to use SciPy ODE solver
    
    * Add code to collect data
    
    * Add H1 error
    
    * Change L2 error
    
    * Add convergence plot
    
    * Fix conflict
    
    * Refactor code
    
    * Remove and refactor code
    
    * Remove csv and notebooks
    
    * Update code
    
    * Add Base class
    
    * Add LinearEquispaced class
    
    * Refactor code
    
    * Return number of step
    
    * Change cell_type to ufl_cell
    
    * Fix typo
    
    * Fix typo
    
    * Flake8 fix
    
    * Add SI unit
    
    * Update .gitignore
    
    * Add lossy models
    
    * Add Westervelt model
    
    * flake8 fix
    
    * Remove unused variable
    
    * Update tests
    
    * Update .gitignore
    
    * Untrack files
    
    * Flake8 fix
    
    * Update .gitignore
    
    * Flake8 fix
    
    * Remove unused variable
    
    * Update tests
    
    * Flake8 fix
    
    * More flake8 fix

---
+++ test/test_element_mass_matrix.py
@@ -0,0 +1,65 @@
+import numpy as np
+from mpi4py import MPI
+
+import dolfinx
+import dolfinx.cpp
+import dolfinx.fem
+import ufl
+
+
+def generate_mesh(dimension):
+    """
+    Generate mesh for testing purposes.
+    """
+
+    if dimension == 1:
+        # Interval mesh
+        mesh = dolfinx.IntervalMesh(
+            MPI.COMM_WORLD,
+            1,
+            [-1, 1]
+        )
+    elif dimension == 2:
+        # Quad mesh
+        mesh = dolfinx.RectangleMesh(
+            MPI.COMM_WORLD,
+            [np.array([-1., -1., 0.]), np.array([1., 1., 0.])],
+            [1, 1],
+            cell_type=dolfinx.cpp.mesh.CellType.quadrilateral
+        )
+    elif dimension == 3:
+        # Hex mesh
+        mesh = dolfinx.BoxMesh(
+            MPI.COMM_WORLD,
+            [np.array([-1., -1., -1.]), np.array([1., 1., 1.])],
+            [1, 1, 1],
+            cell_type=dolfinx.cpp.mesh.CellType.hexahedron
+        )
+    else:
+        raise Exception("Dimension {} is not a valid \
+                        dimension!".format(dimension))
+
+    return mesh
+
+
+# Choose mesh
+mesh = generate_mesh(2)
+
+# Create function space
+p = 3
+FE = ufl.FiniteElement("Lagrange", ufl.quadrilateral, p, variant="gll")
+V = dolfinx.FunctionSpace(mesh, FE)
+
+# Define variational form
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)
+
+a = u*v*ufl.dx(metadata={"quadrature_rule": "GLL", "quadrature_degree": p+1})
+
+# Build element mass matrix
+A = dolfinx.fem.assemble_matrix(a)
+A.assemble()
+
+# Print element mass matrix
+print("Me = ")
+print(A[:, :])
---
+++ test/test_linear_1d.py
@@ -0,0 +1,103 @@
+import numpy as np
+from mpi4py import MPI
+
+from dolfinx import IntervalMesh, FunctionSpace, Function
+from dolfinx.fem import assemble_scalar
+from dolfinx.mesh import locate_entities_boundary, MeshTags
+from ufl import inner, dx
+
+from models import LinearGLL
+from rk import solve_ibvp
+
+# Material parameters
+c0 = 1  # speed of sound (m/s)
+rho0 = 1  # density of medium (kg / m^3)
+
+# Source parameters
+f0 = 10  # source frequency (Hz)
+w0 = 2 * np.pi * f0  # angular frequency (rad / s)
+u0 = 1  # velocity amplitude (m / s)
+p0 = rho0*c0*u0  # pressure amplitude (Pa)
+
+# Domain parameters
+L = 1.0  # domain length (m)
+
+# Physical parameters
+lmbda = c0/f0  # wavelength (m)
+k = 2 * np.pi / lmbda  # wavenumber (m^-1)
+
+# FE parameters
+degree = 4  # degree of basis function
+
+# Mesh parameters
+epw = 4  # number of element per wavelength
+nw = L / lmbda  # number of waves
+nx = int(epw * nw + 1)  # total number of elements
+h = L / nx
+
+# Generate mesh
+mesh = IntervalMesh(MPI.COMM_WORLD, nx, [0, L])
+
+# Tag boundaries
+tdim = mesh.topology.dim
+
+facets0 = locate_entities_boundary(
+    mesh, tdim-1, lambda x: x[0] < np.finfo(float).eps)
+facets1 = locate_entities_boundary(
+    mesh, tdim-1, lambda x: x[0] > L - np.finfo(float).eps)
+
+indices, pos = np.unique(np.hstack((facets0, facets1)), return_index=True)
+values = np.hstack((np.full(facets0.shape, 1, np.intc),
+                    np.full(facets1.shape, 2, np.intc)))
+mt = MeshTags(mesh, tdim-1, indices, values[pos])
+
+# Temporal parameters
+tstart = 0.0  # simulation start time (s)
+tend = L / c0 + 16 / f0  # simulation final time (s)
+tspan = [tstart, tend]
+
+CFL = 0.9
+dt = CFL * h / (c0 * degree**2)
+
+print("Final time:", tend)
+
+# Instantiate model
+eqn = LinearGLL(mesh, mt, degree, c0, f0, p0)
+print("Degree of freedoms: ", eqn.V.dofmap.index_map.size_global)
+
+# Solve
+u, tf, nstep = solve_ibvp(eqn.f0, eqn.f1, *eqn.init(), dt, tspan, 4)
+
+
+# Calculate L2 and H1 errors of FEM solution and best approximation
+class Analytical:
+    def __init__(self, c0, f0, p0, t):
+        self.p0 = p0
+        self.c0 = c0
+        self.f0 = f0
+        self.w0 = 2 * np.pi * f0
+        self.t = t
+
+    def __call__(self, x):
+        val = self.p0 * np.sin(self.w0 * (self.t - x[0]/self.c0)) * \
+              np.heaviside(self.t-x[0]/self.c0, 0)
+
+        return val
+
+
+V_e = FunctionSpace(mesh, ("Lagrange", degree+3))
+u_e = Function(V_e)
+u_e.interpolate(Analytical(c0, f0, p0, tf))
+
+# L2 error
+diff = u - u_e
+L2_diff = mesh.mpi_comm().allreduce(
+    assemble_scalar(inner(diff, diff) * dx), op=MPI.SUM)
+L2_exact = mesh.mpi_comm().allreduce(
+    assemble_scalar(inner(u_e, u_e) * dx), op=MPI.SUM)
+
+L2_error = abs(np.sqrt(L2_diff) / np.sqrt(L2_exact))
+
+
+def test_L2_error():
+    assert(L2_error < 1E-3)
