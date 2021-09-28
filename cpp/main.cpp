6d3beb80d1ff75bff90617f8adaa1d22cdb3222a
adeebkor <mabm4@cam.ac.uk>
2021-09-28 15:07:12 +0100

    Initial commit

---
+++ cpp/main.cpp
@@ -0,0 +1,19 @@
+#include "forms.h"
+#include <cmath>
+#include <dolfinx.h>
+#include <dolfinx/io/XDMFFile.h>
+#include <dolfinx/fem/Constant.h>
+#include <dolfinx/fem/petsc.h>
+#include <xtensor/xarray.hpp>
+#include <xtensor/xview.hpp>
+
+using namespace dolfinx;
+using namespace dolfinx::io;
+
+int main(int argc, char* argv[])
+{
+    common::subsystem::init_logging(argc, argv);
+    common::subsystem::init_petsc(argc, argv);
+
+    MPI_Comm mpi_comm = MPI_COMM_WORLD;
+}
