db1ab5474b5c5662b859a62dae4b4d680d3ef53c
adeebkor <mabm4@cam.ac.uk>
2022-01-27 18:29:05 +0000

    Organise solvers and add new Westervelt SciPy solver

--- python/src/hifusim/__init__.py
+++ python/src/hifusim/__init__.py
@@ -1,8 +1,8 @@
 from .utils import compute_eval_params  # noqa
-from ._linear import Linear  # noqa
-from ._westervelt import Westervelt  # noqa
-from ._linear_scipy import LinearSciPy  # noqa
+from ._linear import Linear, LinearSciPy  # noqa
+from ._westervelt import Westervelt, WesterveltSciPy  # noqa
 
 __all__ = [
-    "compute_eval_params", "Linear", "Westervelt", "LinearSciPy"
+    "compute_eval_params", "Linear", "LinearSciPy", "Westervelt",
+    "WesterveltScipy"
 ]
