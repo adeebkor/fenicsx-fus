76ae83deedfb99eafcc8cd80c8314219effa4fd8
adeebkor <mabm4@cam.ac.uk>
2022-02-01 18:58:12 +0000

    Tidy up

--- python/src/hifusim/__init__.py
+++ python/src/hifusim/__init__.py
@@ -3,6 +3,7 @@ from ._linear import Linear, LinearGLL, LinearGLLSciPy  # noqa
 from ._westervelt import Westervelt, WesterveltGLL, WesterveltGLLSciPy  # noqa
 
 __all__ = [
-    "compute_eval_params", "Linear", "LinearGLL", "LinearGLLSciPy",
-    "Westervelt", "WesterveltGLL", "WesterveltGLLSciPy"
+    "compute_eval_params",
+    "Linear", "LinearGLL", "LinearGLLSciPy",
+    "Westervelt", "WesterveltGLL", "WesterveltGLLSciPy",
 ]
