a1fb50a864b62011afca4f89b6fff80904fea33f
adeebkor <mabm4@cam.ac.uk>
2022-02-21 15:01:39 +0000

    Remove commented lines

--- cpp/common/LinearGLL.hpp
+++ cpp/common/LinearGLL.hpp
@@ -119,7 +119,6 @@ public:
     // Allocate memory for the RHS
     b = std::make_shared<la::Vector<double>>(index_map, bs);
     _b = b->mutable_array();
-
   }
 
   // Set the initial values of u and v, i.e. u_0 and v_0
@@ -210,13 +209,13 @@ public:
     xt::xtensor<double, 2>::shape_type sh0 = {1, 3};
     auto points_on_proc = xt::empty<double>(sh0);
 
-    for (int i = 0; i < N; i++){
-        auto link = colliding_cells.links(i);
-        if (link.size() > 0){
-            auto p = xt::view(pointsT, i, xt::newaxis(), xt::all());
-            points_on_proc = xt::vstack(xt::xtuple(points_on_proc, p));
-            cells.push_back(link[0]);
-        }
+    for (int i = 0; i < N; i++) {
+      auto link = colliding_cells.links(i);
+      if (link.size() > 0) {
+        auto p = xt::view(pointsT, i, xt::newaxis(), xt::all());
+        points_on_proc = xt::vstack(xt::xtuple(points_on_proc, p));
+        cells.push_back(link[0]);
+      }
     }
 
     points_on_proc = xt::view(points_on_proc, xt::drop(0), xt::all());
@@ -320,26 +319,26 @@ public:
         }
       }
 
-      if (t > (0.1 / c0_) + 4.0 / freq0_ && nstep_period < 4*numStepPerPeriod){
-          kernels::copy(*u_, *u_n->x());
-          u_n->x()->scatter_fwd();
-          u_n->eval(points_on_proc, cells, u_eval);
-          uval = u_eval.data();
-          dt = T_ / numStepPerPeriod;
-          MPI_Barrier(MPI_COMM_WORLD);
+      if (t > (0.1 / c0_) + 4.0 / freq0_ && nstep_period < 4 * numStepPerPeriod) {
+        kernels::copy(*u_, *u_n->x());
+        u_n->x()->scatter_fwd();
+        u_n->eval(points_on_proc, cells, u_eval);
+        uval = u_eval.data();
+        dt = T_ / numStepPerPeriod;
+        MPI_Barrier(MPI_COMM_WORLD);
 
-          for (int i = 0; i < size; i++){
-              if (rank == i){
-                  fname = "pressure_on_z_axis_" + std::to_string(nstep_period) + ".txt";
-                  std::ofstream outfile(fname, std::ios_base::app);
-                  for (int i = 0; i < lsize; i++){
-                      outfile << *(pval + 3*i + 2) << "," << *(uval + i) << std::endl;
-                  }
-                  outfile.close();
-              }
-              MPI_Barrier(MPI_COMM_WORLD);
+        for (int i = 0; i < size; i++) {
+          if (rank == i) {
+            fname = "pressure_on_z_axis_" + std::to_string(nstep_period) + ".txt";
+            std::ofstream outfile(fname, std::ios_base::app);
+            for (int i = 0; i < lsize; i++) {
+              outfile << *(pval + 3 * i + 2) << "," << *(uval + i) << std::endl;
+            }
+            outfile.close();
           }
-          nstep_period++;
+          MPI_Barrier(MPI_COMM_WORLD);
+        }
+        nstep_period++;
       }
     }
 
@@ -353,124 +352,7 @@ public:
     u_n->x()->scatter_fwd();
     v_n->x()->scatter_fwd();
 
-    io::XDMFFile file_solution(mesh->comm(), "u.xdmf", "w");
-    file_solution.write_mesh(*mesh);
-    file_solution.write_function(*u_n, t);
-
-    // --------------------------------------------------------------------- //
-    // Evaluate on a line
-    int N = 20000;
-    double tol = 1e-6;
-    xt::xarray<double> zp = xt::linspace<double>(tol, 0.1 - tol, N);
-    zp.reshape({1, N});
-    auto xyp = xt::zeros<double>({2, N});
-    auto points = xt::vstack(xt::xtuple(xyp, zp));
-    auto pointsT = xt::transpose(points);
-
-    auto bb_tree = geometry::BoundingBoxTree(*mesh, mesh->topology().dim());
-    auto cell_candidates = compute_collisions(bb_tree, pointsT);
-
-    auto colliding_cells = geometry::compute_colliding_cells(*mesh, cell_candidates, pointsT);
-
-    std::vector<int> cells;
-    xt::xtensor<double, 2>::shape_type sh0 = {1, 3};
-    auto points_on_proc = xt::empty<double>(sh0);
-
-    int local_size = mesh->topology().index_map(2)->size_local();
-
-    for (int i = 0; i < N; i++) {
-      auto link = colliding_cells.links(i);
-      if (link.size() > 0) {
-        auto p = xt::view(pointsT, i, xt::newaxis(), xt::all());
-        points_on_proc = xt::vstack(xt::xtuple(points_on_proc, p));
-        cells.push_back(link[0]);
-      }
-    }
-
-    points_on_proc = xt::view(points_on_proc, xt::drop(0), xt::all());
-    int lsize = points_on_proc.shape(0);
-    xt::xtensor<double, 2> u_eval({lsize, 1});
-
-    u_n->eval(points_on_proc, cells, u_eval);
-
-    double* uval = u_eval.data();
-    double* pval = points_on_proc.data();
-
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    for (int i = 0; i < size; i++) {
-      if (rank == i) {
-        std::ofstream outfile("pressure_on_z_axis.txt", std::ios_base::app);
-        for (int i = 0; i < lsize; i++) {
-          outfile << *(pval + 3 * i + 2) << "," << *(uval + i) << std::endl;
-        }
-        outfile.close();
-      }
-      MPI_Barrier(MPI_COMM_WORLD);
-    }
-
-    // --------------------------------------------------------------------- //
-    // Evaluate on a line
-    // int N = 20000;
-    // double tol = 1e-6;
-    // xt::xarray<double> zp = xt::linspace<double>(tol, 0.1 - tol, N);
-    // zp.reshape({1, N});
-    // auto xyp = xt::zeros<double>({2, N});
-    // auto points = xt::vstack(xt::xtuple(xyp, zp));
-    // auto pointsT = xt::transpose(points);
-
-    // auto bb_tree = geometry::BoundingBoxTree(*mesh, mesh->topology().dim());
-    // auto cell_candidates = compute_collisions(bb_tree, pointsT);
-
-    // auto colliding_cells = geometry::compute_colliding_cells(*mesh, cell_candidates, pointsT);
-
-    // std::vector<int> cells;
-    // xt::xtensor<double, 2>::shape_type sh0 = {1, 3};
-    // auto points_on_proc = xt::empty<double>(sh0);
-
-    // int local_size = mesh->topology().index_map(2)->size_local();
-
-    // for (int i = 0; i < N; i++) {
-    //   auto link = colliding_cells.links(i);
-    //   if (link.size() > 0) {
-    //     auto p = xt::view(pointsT, i, xt::newaxis(), xt::all());
-    //     points_on_proc = xt::vstack(xt::xtuple(points_on_proc, p));
-    //     cells.push_back(link[0]);
-    //   }
-    // }
-
-    // points_on_proc = xt::view(points_on_proc, xt::drop(0), xt::all());
-    // int lsize = points_on_proc.shape(0);
-    // xt::xtensor<double, 2> u_eval({lsize, 1});
-
-    // u_n->eval(points_on_proc, cells, u_eval);
-
-    // double* uval = u_eval.data();
-    // double* pval = points_on_proc.data();
-
-    // int rank, size;
-    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    // MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    // for (int i = 0; i < size; i++) {
-    //   if (rank == i) {
-    //     std::ofstream outfile("pressure_on_z_axis.txt", std::ios_base::app);
-    //     for (int i = 0; i < lsize; i++) {
-    //       outfile << *(pval + 3 * i + 2) << "," << *(uval + i) << std::endl;
-    //     }
-    //     outfile.close();
-    //   }
-    //   MPI_Barrier(MPI_COMM_WORLD);
-    // }
-
-<<<<<<< Updated upstream
-    // --------------------------------------------------------------------- //
-
-=======
     // file.write(t);
     // file.close();
->>>>>>> Stashed changes
   }
 };
