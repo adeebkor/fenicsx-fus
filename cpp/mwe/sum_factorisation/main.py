bf48f67d2453bb646f41f30ab1e76e8dfa5833b9
adeebkor <mabm4@cam.ac.uk>
2022-05-24 09:41:02 +0100

    Use tensordot

--- cpp/mwe/sum_factorisation/main.py
+++ cpp/mwe/sum_factorisation/main.py
@@ -6,7 +6,7 @@ N = 2
 xi = np.arange(N*N*N).reshape(N, N, N)
 phi = np.arange(M*N).reshape(M, N)
 
-T = np.einsum('iq, qjk', phi, xi)
+T = np.tensordot(phi, xi, axes=[1, 0])
 
 print("T = \n", T)
 print("T transpose [2, 0, 1] = \n",  np.transpose(T, [2, 0, 1]))
