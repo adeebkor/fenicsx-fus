708e7ccd30a5559a248c9d51a8639614a8764bad
adeebkor <mabm4@cam.ac.uk>
2022-06-15 14:06:44 +0100

    Complete implementation of LinearGLLPenetrable

--- cpp/common/LinearGLLPenetrable.hpp
+++ cpp/common/LinearGLLPenetrable.hpp
@@ -38,7 +38,8 @@ template <int P>
 class LinearGLLPenetrable {
 public:
   LinearGLLPenetrable(std::shared_ptr<mesh::Mesh> Mesh,
-                      std::shared_ptr<mesh::MeshTags<std::int32_t>> Meshtags,
+                      std::shared_ptr<mesh::MeshTags<std::int32_t>> MTcells,
+                      std::shared_ptr<mesh::MeshTags<std::int32_t>> MTfacets,
                       double& speedOfSound_0, double& speedOfSound_1, 
                       double& sourceFrequency, double& pressureAmplitude) {
 
@@ -46,6 +47,8 @@ public:
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     mesh = Mesh;
+    mt_cells = MTcells;
+    mt_facets = MTfacets;
     V = std::make_shared<fem::FunctionSpace>(
         fem::create_functionspace(functionspace_form_forms_a, "u", Mesh));
     V_DG = std::make_shared<fem::FunctionSpace>(
@@ -53,6 +56,9 @@ public:
 
     index_map = V->dofmap()->index_map;
     bs = V->dofmap()->index_map_bs();
+
+    index_map_DG = V_DG->dofmap()->index_map;
+    bs_DG = V_DG->dofmap()->index_map_bs();
 
     c0 = std::make_shared<fem::Function<double>>(V_DG);
     u = std::make_shared<fem::Function<double>>(V);
@@ -72,7 +78,270 @@ public:
     T_ = 1.0 / freq0_;
     alpha_ = 4.0;
 
+    // Create LHS form
+    xtl::span<double> _u = u->x()->mutable_array();
+    std::fill(_u.begin(), _u.end(), 1.0);
+
+    a = std::make_shared<fem::Form<double>>(
+        fem::create_form<double>(*form_forms_a, {V}, {{"u", u}}, {}, {}));
+
+    m = std::make_shared<la::Vector<double>>(index_map, bs);
+    _m = m->mutable_array();
+    std::fill(_m.begin(), _m.end(), 0.0);
+    fem::assemble_vector(_m, *a);
+    m->scatter_rev(common::IndexMap::Mode::add);
+
+    // Fill the speed of sound DG function with the appropriate values
+    auto cells_1 = mt_cells->find(1);
+    auto cells_2 = mt_cells->find(2);
+    _c0 = c0->x()->mutable_array();
+    std::for_each(cells_1.begin(), cells_1.end(),
+        [&](std::int32_t &i) { _c0[i] = speedOfSound_0; });
+    std::for_each(cells_2.begin(), cells_2.end(),
+        [&](std::int32_t &i) { _c0[i] = speedOfSound_1; });
+    c0->x()->scatter_fwd();
+
+    // Create RHS form
+    L = std::make_shared<fem::Form<double>>(
+        fem::create_form<double>(*form_forms_L, {V}, 
+        {{"u_n", u_n}, {"g", g}, {"v_n", v_n}, {"c0", c0}}, {},
+        {{dolfinx::fem::IntegralType::exterior_facet, &(*mt_facets)}}));
+    
+    b = std::make_shared<la::Vector<double>>(index_map, bs);
+    _b = b->mutable_array();
   }
+
+  // Set the initial values of u and v, i.e. u_0 and v_0
+  void init() {
+    u_n->x()->set(0.0);
+    v_n->x()->set(0.0);
+  }
+
+  /// Evaluate du/dt = f0(t, u, v)
+  /// @param[in] t Current time, i.e. tn
+  /// @param[in] u Current u, i.e. un
+  /// @param[in] v Current v, i.e. vn
+  /// @param[out] result Result, i.e. dun/dtn
+  void f0(double& t, std::shared_ptr<la::Vector<double>> u, std::shared_ptr<la::Vector<double>> v,
+          std::shared_ptr<la::Vector<double>> result) {
+    kernels::copy(*v, *result);
+  }
+
+  /// Evaluate dv/dt = f1(t, u, v)
+  /// @param[in] t Current time, i.e. tn
+  /// @param[in] u Current u, i.e. un
+  /// @param[in] v Current v, i.e. vn
+  /// @param[out] result Result, i.e. dvn/dtn
+  void f1(double& t, std::shared_ptr<la::Vector<double>> u, std::shared_ptr<la::Vector<double>> v,
+          std::shared_ptr<la::Vector<double>> result) {
+
+    // Apply windowing
+    if (t < T_ * alpha_) {
+      window_ = 0.5 * (1.0 - cos(freq0_ * M_PI * t / alpha_));
+    } else {
+      window_ = 1.0;
+    }
+
+    // Update boundary condition
+    std::fill(_g.begin(), _g.end(), window_ * p0_ * w0_ / c0_ * cos(w0_ * t));
+
+    u->scatter_fwd();
+    kernels::copy(*u, *u_n->x());
+
+    v->scatter_fwd();
+    kernels::copy(*v, *v_n->x());
+
+    // Assemble RHS
+    std::fill(_b.begin(), _b.end(), 0.0);
+
+    fem::assemble_vector(_b, *L);
+    b->scatter_rev(common::IndexMap::Mode::add);
+
+    // Solve
+    // TODO: Divide is more expensive than multiply.
+    // We should store the result of 1/m in a vector and apply and element wise vector
+    // multiplication, since m doesn't change for linear wave propagation.
+    {
+      out = result->mutable_array();
+      b_ = b->array();
+      m_ = m->array();
+
+      // Element wise division
+      // out[i] = b[i]/m[i]
+      std::transform(b_.begin(), b_.end(), m_.begin(), out.begin(),
+                     [](const double& bi, const double& mi) { return bi / mi; });
+    }
+  }
+
+  /// Runge-Kutta 4th order solver
+  /// @param[in] startTime initial time of the solver
+  /// @param[in] finalTime final time of the solver
+  /// @param[in] timeStep  time step size of the solver
+  void rk4(double& startTime, double& finalTime, double& timeStep) {
+    
+    // ------------------------------------------------------------------------
+    // Computing function evaluation parameters
+
+    std::string fname;
+
+    // Grid parameters
+    int N = 2048;
+    double tol = 1e-6;
+
+    // Generate evaluate points
+    xt::xarray<double> zp = xt::linspace<double>(tol, 0.12, N);
+    zp.reshape({1, N});
+    auto xyp = xt::zeros<double>({2, N});
+    auto points = xt::vstack(xt::xtuple(xyp, zp));
+    auto pointsT = xt::transpose(points);
+
+    // Compute evaluation parameters
+    auto bb_tree = geometry::BoundingBoxTree(*mesh, mesh->topology().dim());
+    auto cell_candidates = compute_collisions(bb_tree, pointsT);
+    auto colliding_cells = geometry::compute_colliding_cells(
+        *mesh, cell_candidates, pointsT);
+
+    std::vector<int> cells;
+    xt::xtensor<double, 2>::shape_type sh0 = {1, 3};
+    auto points_on_proc = xt::empty<double>(sh0);
+    for (int i = 0; i < N; i++) {
+      auto link = colliding_cells.links(i);
+      if (link.size() > 0) {
+        auto p = xt::view(pointsT, i, xt::newaxis(), xt::all());
+        points_on_proc = xt::vstack(xt::xtuple(points_on_proc, p));
+        cells.push_back(link[0]);
+      }
+    }
+
+    points_on_proc = xt::view(points_on_proc, xt::drop(0), xt::all());
+    std::size_t lsize = points_on_proc.shape(0);
+    xt::xtensor<double, 2> u_eval({lsize, 1});
+
+    double* uval;
+    double* pval = points_on_proc.data();
+
+    // Time-stepping parameters
+    double t = startTime;
+    double tf = finalTime;
+    double dt = timeStep;
+    int step = 0;
+    int nstep = (finalTime - startTime) / timeStep + 1;
+    int numStepPerPeriod = T_ / dt + 1;
+    int nstep_period = 0;
+
+    // Time-stepping vectors
+    std::shared_ptr<la::Vector<double>> u_, v_, un, vn, u0, v0, ku, kv;
+
+    // Placeholder vectors at time step n
+    u_ = std::make_shared<la::Vector<double>>(index_map, bs);
+    v_ = std::make_shared<la::Vector<double>>(index_map, bs);
+
+    kernels::copy(*u_n->x(), *u_);
+    kernels::copy(*v_n->x(), *v_);
+
+    // Placeholder vectors at intermediate time step n
+    un = std::make_shared<la::Vector<double>>(index_map, bs);
+    vn = std::make_shared<la::Vector<double>>(index_map, bs);
+
+    // Placeholder vectors at start of time step
+    u0 = std::make_shared<la::Vector<double>>(index_map, bs);
+    v0 = std::make_shared<la::Vector<double>>(index_map, bs);
+
+    // Placeholder at k intermediate time step
+    ku = std::make_shared<la::Vector<double>>(index_map, bs);
+    kv = std::make_shared<la::Vector<double>>(index_map, bs);
+
+    kernels::copy(*u_, *ku);
+    kernels::copy(*v_, *kv);
+
+    // Runge-Kutta timestepping data
+    int n_RK = 4;
+    xt::xarray<double> a_runge{0.0, 0.5, 0.5, 1.0};
+    xt::xarray<double> b_runge{1.0 / 6.0, 1.0 / 3.0, 1.0 / 3.0, 1.0 / 6.0};
+    xt::xarray<double> c_runge{0.0, 0.5, 0.5, 1.0};
+
+    // RK variables
+    double tn;
+
+    while (t < tf) {
+      dt = std::min(dt, tf - t);
+
+      // Store solution at start of time step
+      kernels::copy(*u_, *u0);
+      kernels::copy(*v_, *v0);
+
+      // Runge-Kutta step
+      for (int i = 0; i < n_RK; i++) {
+        kernels::copy(*u0, *un);
+        kernels::copy(*v0, *vn);
+
+        kernels::axpy(*un, dt * a_runge(i), *ku, *un);
+        kernels::axpy(*vn, dt * a_runge(i), *kv, *vn);
+
+        // RK time evaluation
+        tn = t + c_runge(i) * dt;
+
+        // Compute RHS vector
+        f0(tn, un, vn, ku);
+        f1(tn, un, vn, kv);
+
+        // Update solution
+        kernels::axpy(*u_, dt * b_runge(i), *ku, *u_);
+        kernels::axpy(*v_, dt * b_runge(i), *kv, *v_);
+      }
+
+      // Update time
+      t += dt;
+      step += 1;
+
+      if (step % 10 == 0) {
+        if (rank == 0) {
+          std::cout << "t: " << t 
+                    << ",\t Steps: " << step 
+                    << "/" << nstep << std::endl;
+        }
+      }
+
+      // Collect data for one period
+      if (t > 0.12 / c0_ + 6.0 / freq0_ && nstep_period < numStepPerPeriod) {
+        kernels::copy(*u_, *u_n->x());
+        u_n->x()->scatter_fwd();
+
+        // Function evaluation
+        u_n->eval(points_on_proc, cells, u_eval);
+        uval = u_eval.data();
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        // Write evaluation from each process to a single text file
+        for (int i = 0; i < size; i++) {
+          if (rank == i) {
+            fname = "/home/mabm4/rds/data/pressure_on_z_axis_" + 
+                    std::to_string(nstep_period) + ".txt";
+            std::ofstream txt_file(fname, std::ios_base::app);
+            for (std::size_t i = 0; i < lsize; i++) {
+              txt_file << *(pval + 3 * i + 2) << "," 
+                      << *(uval + i) << std::endl;
+            }
+            txt_file.close();
+          }
+          MPI_Barrier(MPI_COMM_WORLD);
+        }
+        nstep_period++;
+      }
+    }
+
+    // Prepare solution at final time
+    kernels::copy(*u_, *u_n->x());
+    kernels::copy(*v_, *v_n->x());
+    u_n->x()->scatter_fwd();
+    v_n->x()->scatter_fwd();
+
+  }
+
+  std::size_t num_dofs() const {
+    return V->dofmap()->index_map->size_global();
+  }
+
 
 private:
   int rank, size;  // MPI rank and size
@@ -85,16 +354,17 @@ private:
   double window_;
 
   std::shared_ptr<mesh::Mesh> mesh;
+  std::shared_ptr<mesh::MeshTags<std::int32_t>> mt_cells, mt_facets;
   std::shared_ptr<fem::FunctionSpace> V, V_DG;
   std::shared_ptr<fem::Function<double>> c0;
   std::shared_ptr<fem::Function<double>> u, v, g, u_n, v_n;
   std::shared_ptr<fem::Form<double>> a, L;
-  std::shared_ptr<la::Vector<double>> m, b;
+  std::shared_ptr<la::Vector<double>> m, b, c_0;
 
-  xtl::span<double> _g, out;
+  xtl::span<double> _c0, _g, out;
   xtl::span<const double> m_, b_;
   xtl::span<double> _m, _b;
 
-  std::shared_ptr<const common::IndexMap> index_map;
-  int bs;
+  std::shared_ptr<const common::IndexMap> index_map, index_map_DG;
+  int bs, bs_DG;
 };
--- cpp/experiments/exp2/main.cpp
+++ cpp/experiments/exp2/main.cpp
@@ -25,27 +25,29 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     // Material parameters
-    constexpr double speedOfSoundWater = 1500.0;  // (m/s)
-    constexpr double speedOfSoundBone = 2800.0;   // (m/s)
+    double speedOfSoundWater = 1500.0;  // (m/s)
+    double speedOfSoundBone = 2800.0;   // (m/s)
 
     // Source parameters
-    constexpr double sourceFrequency = 0.5e6;      // (Hz)
-    constexpr double pressureAmplitude = 60000.0;  // (Pa)
-    constexpr double period = 1 / sourceFrequency; // (s)
+    double sourceFrequency = 0.5e6;      // (Hz)
+    double pressureAmplitude = 60000.0;  // (Pa)
+    double period = 1 / sourceFrequency; // (s)
 
     // Domain parameters
-    constexpr double domainLength = 0.12; // (m)
+    double domainLength = 0.12; // (m)
 
     // FE parameters
-    constexpr int degreeOfBasis = 4;
+    const int degreeOfBasis = 4;
 
     // Read mesh and mesh tags
     auto element = fem::CoordinateElement(mesh::CellType::hexahedron, 1);
     io::XDMFFile xdmf(MPI_COMM_WORLD, "/home/mabm4/rds/hpc-work/big_mesh/mesh.xdmf", "r");
     auto mesh
-        = std::make_shared<mesh::Mesh>(xdmf.read_mesh(element, mesh::GhostMode::none, "planar3d"));
+        = std::make_shared<mesh::Mesh>(xdmf.read_mesh(element, mesh::GhostMode::none, "planar3d_penetrable"));
     mesh->topology().create_connectivity(1, 2);
-    auto mt = std::make_shared<mesh::MeshTags<std::int32_t>>(
+    auto mt_cells = std::make_shared<mesh::MeshTags<std::int32_t>>(
+        xdmf.read_meshtags(mesh, "planar3d_regions"));
+    auto mt_facets = std::make_shared<mesh::MeshTags<std::int32_t>>(
         xdmf.read_meshtags(mesh, "planar3d_boundaries"));
 
     // Get smallest mesh size
@@ -71,20 +73,43 @@ int main(int argc, char* argv[]) {
     double CFL = 0.64;
     double timeStepSize = CFL * meshSize / (speedOfSoundBone * pow(degreeOfBasis, 2));
     double startTime = 0.0;
-    double finalTime = 20 * timeStepSize; // domainLength / speedOfSound + 8.0 / sourceFrequency;
+    double finalTime = domainLength / speedOfSoundWater + 8.0 / sourceFrequency;
     int stepPerPeriod = period / timeStepSize + 1;
     timeStepSize = period / stepPerPeriod;
     int numberOfStep = finalTime / timeStepSize + 1;
+
     if (rank == 0) {
       std::cout << "Number of step per period: " << stepPerPeriod << std::endl;
       std::cout << "dt = " << timeStepSize << std::endl;
     }
 
     // Model
+    common::Timer instantiation("~ Instantiate model");
+    instantiation.start();
+    auto eqn = LinearGLLPenetrable<degreeOfBasis>(mesh, mt_cells, mt_facets, 
+    speedOfSoundWater, speedOfSoundBone, sourceFrequency, pressureAmplitude);
+    instantiation.stop();
 
+    if (rank == 0) {
+      std::cout << "CFL number: " << CFL << std::endl;
+      std::cout << "Number of steps: " << numberOfStep << std::endl;
+      std::cout << "Degrees of freedom: " << eqn.num_dofs() << std::endl;
+    }
 
     // Solve
+    eqn.init();
     
+    common::Timer rksolve("~ RK solve time");
+    rksolve.start();
+    eqn.rk4(startTime, finalTime, timeStepSize);
+    rksolve.stop();
+
+    if (rank == 0) {
+      std::cout << "Solve time: " << rksolve.elapsed()[0] << std::endl;
+      std::cout << "Time per step: " << rksolve.elapsed()[0] / numberOfStep << std::endl;
+    }
+
+    list_timings(MPI_COMM_WORLD, {TimingType::wall}, Table::Reduction::min);
   }
   PetscFinalize();
   return 0;
