83743e95b51bcf0d8067825806dafaaec9eb3c9d
adeebkor <mabm4@cam.ac.uk>
2022-08-14 15:12:41 +0100

    Start on lossy_planar2d

--- python/examples/lossy_planar2d/lossy_planar2d.py
+++ python/examples/lossy_planar2d/lossy_planar2d.py
@@ -11,4 +11,19 @@ from mpi4py import MPI
 from dolfinx.io import XDMFFile
 from dolfinx import cpp
 
-from hifusim import LossyGLL
+from hifusim import LossyGLL, compute_diffusivity_of_sound
+
+# MPI
+mpi_rank = MPI.COMM_WORLD.rank
+mpi_size = MPI.COMM_WORLD.size
+
+# Source parameters
+sourceFrequency = 0.5e6  # (Hz)
+sourceAmplitude = 60000  # (Hz)
+period = 1 / sourceFrequency  # (s)
+
+# Material parameters
+speedOfSound = 1500  # (m/s)
+attenuationCoefficientdB = 100.0  # (dB/m)
+diffusivityOfSound = compute_diffusivity_of_sound(
+    sourceFrequency, speedOfSound, attenuationCoefficientdB)
