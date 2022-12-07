522e965adfe7c5521731809adee0ec867f1c934b
adeebkor <mabm4@cam.ac.uk>
2022-12-07 17:33:53 +0000

    Add timings

--- cpp/fenicsx/examples/linear_planewave2d_1/main.cpp
+++ cpp/fenicsx/examples/linear_planewave2d_1/main.cpp
@@ -120,8 +120,19 @@ int main(int argc, char* argv[])
       speedOfSound);
 
     // Solve
+    common::Timer tsolve("Solve time");
+
     model.init();
+    
+    tsolve.start();
     model.rk4(startTime, finalTime, timeStepSize);
+    tsolve.stop();
+
+    if (mpi_rank == 0) {
+      std::cout << "Solve time: " << tsolve.elapsed()[0] << std::endl;
+      std::cout << "Time per step: " 
+                << tsolve.elapsed()[0] / numberOfStep << std::endl;
+    }
 
     // Final solution
     auto u_n = model.u_sol();
