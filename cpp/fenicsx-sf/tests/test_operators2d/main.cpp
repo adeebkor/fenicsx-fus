fbd234959487b9b798c036c0393283050eb0e012
adeebkor <mabm4@cam.ac.uk>
2022-12-08 16:12:00 +0000

    Remove whitespace

--- cpp/fenicsx-sf/tests/test_operators2d/main.cpp
+++ cpp/fenicsx-sf/tests/test_operators2d/main.cpp
@@ -19,14 +19,12 @@ int main(int argc, char* argv[])
   PetscInitialize(&argc, &argv, nullptr, nullptr);
 
   {
-
     // MPI
-
     int mpi_rank, mpi_size;
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 
-    // Define polynomial degree
+    // Set polynomial degree
     const int P = 4;
 
     // Create mesh and function space
