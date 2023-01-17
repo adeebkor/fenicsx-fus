4ea0230e1c24999c8700114dd712a21e22ff20ff
adeebkor <mabm4@cam.ac.uk>
2023-01-17 18:35:31 +0000

    Change tags for absorbing boundary

--- cpp/fenicsx-sf/benchmarks/SC2-BM1/forms.py
+++ cpp/fenicsx-sf/benchmarks/SC2-BM1/forms.py
@@ -36,8 +36,6 @@ md = {"quadrature_rule": "GLL", "quadrature_degree": qdegree[Q]}
 a = inner(u/rho0/c0/c0, v) * dx(metadata=md)
 
 L = inner(1/rho0*g, v)*ds(1, metadata=md) \
-    - inner(1/rho0/c0*v_n, v)*ds(1, metadata=md) \
-    - inner(1/rho0/c0*v_n, v)*ds(2, metadata=md) \
-    - inner(1/rho0/c0*v_n, v)*ds(3, metadata=md)
+    - inner(1/rho0/c0*v_n, v)*ds(2, metadata=md)
 
 forms = [a, L]
