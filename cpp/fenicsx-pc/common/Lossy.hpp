3bec873294a91ea508ea5c653a0731e1a15a4e86
adeebkor <mabm4@cam.ac.uk>
2023-01-24 16:39:25 +0000

    Implement 2D solver

--- cpp/fenicsx-pc/common/Lossy.hpp
+++ cpp/fenicsx-pc/common/Lossy.hpp
@@ -48,16 +48,17 @@ void axpy(la::Vector<T>& r, T alpha, const la::Vector<T>& x,
 /// @param [in] sourceFrequency The source frequency
 /// @param [in] sourceAmplitude The source amplitude
 /// @param [in] sourceSpeed The medium speed of sound that is in contact with the source
-template <typename T, int P>
-class LossySpectral2D {
+template <typename T, int P, int Q>
+class Lossy2D {
 public:
-  LossySpectral2D(
+  Lossy2D(
     std::shared_ptr<mesh::Mesh> Mesh,
     std::shared_ptr<mesh::MeshTags<std::int32_t>> FacetTags,
     std::shared_ptr<fem::Function<T>> speedOfSound,
     std::shared_ptr<fem::Function<T>> density,
     std::shared_ptr<fem::Function<T>> diffusivityOfSound,
-    const T& sourceFrequency, const T& sourceAmplitude)
+    const T& sourceFrequency, const T& sourceAmplitude,
+    const T& sourceSpeed)
   {
     // MPI
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
@@ -85,6 +86,10 @@ public:
     // Define field functions
     index_map = V->dofmap()->index_map;
     bs = V->dofmap()->index_map_bs();
+
+    u = std::make_shared<fem::Function<T>>(V);
+    u_n = std::make_shared<fem::Function<T>>(V);
+    v_n = std::make_shared<fem::Function<T>>(V);
 
     // Define source function
     g = std::make_shared<fem::Function<T>>(V);
@@ -121,8 +126,8 @@ public:
     b_ = b->mutable_array();
 
     // Define operators
-    lin_op = std::make_shared<StiffnessSpectral2D<T, P>>(V);
-    att_op = std::make_shared<StiffnessSpectral2D<T, P>>(V);
+    lin_op = std::make_shared<Stiffness2D<T, P, Q>>(V);
+    att_op = std::make_shared<Stiffness2D<T, P, Q>>(V);
 
     // Define coefficient for the operators
     std::span<const T> c0_ = c0->x()->array();
@@ -133,8 +138,226 @@ public:
     lin_coeff_ = lin_coeff->x()->mutable_array();
 
     att_coeff = std::make_shared<fem::Function<T>>(rho0->function_space());
-    att_coeff_ = att_coeff
+    att_coeff_ = att_coeff->x()->mutable_array();
 
+    for (std::size_t i = 0; i < rho0_.size(); ++i) {
+      lin_coeff_[i] = - 1.0 / rho0_[i];
+      att_coeff_[i] = - delta0_[i] / rho0_[i] / c0_[i] / c0_[i];
+    }
+
+    lin_coeff->x()->scatter_fwd();
+    att_coeff->x()->scatter_fwd();
+  }
+
+  /// Set the initial values of u and v, i.e. u_0 and v_0
+  void init() {
+    u_n->x()->set(0.0);
+    v_n->x()->set(0.0);
+  }
+
+  /// Evaluate du/dt = f0(t, u, v)
+  /// @param[in] t Current time, i.e. tn
+  /// @param[in] u Current u, i.e. un
+  /// @param[in] v Current v, i.e. vn
+  /// @param[out] result Result, i.e. dun/dtn
+  void f0(T& t, std::shared_ptr<la::Vector<T>> u, std::shared_ptr<la::Vector<T>> v,
+          std::shared_ptr<la::Vector<T>> result) {
+    kernels::copy<T>(*v, *result);
+  }
+
+  /// Evaluate dv/dt = f1(t, u, v)
+  /// @param[in] t Current time, i.e. tn
+  /// @param[in] u Current u, i.e. un
+  /// @param[in] v Current v, i.e. vn
+  /// @param[out] result Result, i.e. dvn/dtn
+  void f1(T& t, std::shared_ptr<la::Vector<T>> u, std::shared_ptr<la::Vector<T>> v,
+          std::shared_ptr<la::Vector<T>> result) {
+
+    // Apply windowing
+    if (t < period * window_length) {
+      window = 0.5 * (1.0 - cos(freq * M_PI * t / window_length));
+      dwindow = 0.5 * M_PI * freq / window_length * sin(freq * M_PI * t 
+                                                         / window_length);
+    } else {
+      window = 1.0;
+      dwindow = 0.0;
+    }
+
+    // Update boundary condition (homogenous domain)
+    // std::fill(g_.begin(), g_.end(), window * p0 * w0 / s0 * cos(w0 * t));
+    // std::fill(dg_.begin(), dg_.end(), 
+    //           dwindow * p0 * w0 / s0 * cos(w0 * t) 
+    //             - window * p0 * w0 * w0 / s0 * sin(w0 * t));
+
+    // Update boundary condition (heterogenous domain)
+    std::fill(g_.begin(), g_.end(), window * 2.0 * p0 * w0 / s0 * cos(w0 * t));
+    std::fill(dg_.begin(), dg_.end(), 
+              dwindow * 2.0 * p0 * w0 / s0 * cos(w0 * t) 
+                - window * 2.0 * p0 * w0 * w0 / s0 * sin(w0 * t));
+
+    // Update fields
+    u->scatter_fwd();
+    kernels::copy<T>(*u, *u_n->x());
+
+    v->scatter_fwd();
+    kernels::copy<T>(*v, *v_n->x());
+
+    // Assemble RHS
+    std::fill(b_.begin(), b_.end(), 0.0);
+    lin_op->operator()(*u_n->x(), lin_coeff_, *b);
+    att_op->operator()(*v_n->x(), att_coeff_, *b);
+    fem::assemble_vector(b_, *L);
+    b->scatter_rev(std::plus<T>());
+
+    // Solve
+    // TODO: Divide is more expensive than multiply.
+    // We should store the result of 1/m in a vector and apply and element wise vector
+    // multiplication, since m doesn't change for linear wave propagation.
+    {
+      out = result->mutable_array();
+      _b = b->array();
+      _m = m->array();
+
+      // Element wise division
+      // out[i] = b[i]/m[i]
+      std::transform(_b.begin(), _b.end(), _m.begin(), out.begin(),
+                     [](const T& bi, const T& mi) { return bi / mi; });
+    }
+  }
+
+  /// Runge-Kutta 4th order solver
+  /// @param[in] startTime initial time of the solver
+  /// @param[in] finalTime final time of the solver
+  /// @param[in] timeStep  time step size of the solver
+  void rk4(const T& startTime, const T& finalTime, const T& timeStep) {
+
+    // Time-stepping parameters
+    T t = startTime;
+    T tf = finalTime;
+    T dt = timeStep;
+    int totalStep = (finalTime - startTime) / timeStep + 1;
+    int step = 0;
+
+    // Time-stepping vectors
+    std::shared_ptr<la::Vector<T>> u_, v_, un, vn, u0, v0, ku, kv;
+
+    // Placeholder vectors at time step n
+    u_ = std::make_shared<la::Vector<T>>(index_map, bs);
+    v_ = std::make_shared<la::Vector<T>>(index_map, bs);
+
+    kernels::copy<T>(*u_n->x(), *u_);
+    kernels::copy<T>(*v_n->x(), *v_);
+
+    // Placeholder vectors at intermediate time step n
+    un = std::make_shared<la::Vector<T>>(index_map, bs);
+    vn = std::make_shared<la::Vector<T>>(index_map, bs);
+
+    // Placeholder vectors at start of time step
+    u0 = std::make_shared<la::Vector<T>>(index_map, bs);
+    v0 = std::make_shared<la::Vector<T>>(index_map, bs);
+
+    // Placeholder at k intermediate time step
+    ku = std::make_shared<la::Vector<T>>(index_map, bs);
+    kv = std::make_shared<la::Vector<T>>(index_map, bs);
+
+    kernels::copy<T>(*u_, *ku);
+    kernels::copy<T>(*v_, *kv);
+
+    // Runge-Kutta 4th order time-stepping data
+    std::array<T, 4> a_runge = {0.0, 0.5, 0.5, 1.0};
+    std::array<T, 4> b_runge = {1.0 / 6.0, 1.0 / 3.0, 1.0 / 3.0, 1.0 / 6.0};
+    std::array<T, 4> c_runge = {0.0, 0.5, 0.5, 1.0};
+
+    // RK variables
+    T tn;
+
+    while (t < tf) {
+      dt = std::min(dt, tf - t);
+
+      // Store solution at start of time step
+      kernels::copy<T>(*u_, *u0);
+      kernels::copy<T>(*v_, *v0);
+
+      // Runge-Kutta 4th order step
+      for (int i = 0; i < 4; i++) {
+        kernels::copy<T>(*u0, *un);
+        kernels::copy<T>(*v0, *vn);
+
+        kernels::axpy<T>(*un, dt * a_runge[i], *ku, *un);
+        kernels::axpy<T>(*vn, dt * a_runge[i], *kv, *vn);
+
+        // RK time evaluation
+        tn = t + c_runge[i] * dt;
+
+        // Compute RHS vector
+        f0(tn, un, vn, ku);
+        f1(tn, un, vn, kv);
+
+        // Update solution
+        kernels::axpy<T>(*u_, dt * b_runge[i], *ku, *u_);
+        kernels::axpy<T>(*v_, dt * b_runge[i], *kv, *v_);
+      }
+
+      // Update time
+      t += dt;
+      step += 1;
+
+      if (step % 100 == 0) {
+        if (mpi_rank == 0) {
+          std::cout << "t: " << t 
+                    << ",\t Steps: " << step 
+                    << "/" << totalStep << std::endl;
+        }
+      }
+    }
+
+    // Prepare solution at final time
+    kernels::copy<T>(*u_, *u_n->x());
+    kernels::copy<T>(*v_, *v_n->x());
+    u_n->x()->scatter_fwd();
+    v_n->x()->scatter_fwd();
 
   }
+
+  std::shared_ptr<fem::Function<T>> u_sol() const {
+    return u_n;
+  }
+
+  std::int64_t number_of_dofs() const {
+    return V->dofmap()->index_map->size_global();
+  }
+
+private:
+  int mpi_rank, mpi_size;  // MPI rank and size
+  int bs;  // block size
+  T freq;  // source frequency (Hz)
+  T p0;  // source amplitude (Pa)
+  T w0;  // angular frequency  (rad/s)
+  T s0;  // speed (m/s)
+  T period, window_length, window, dwindow;
+
+  std::shared_ptr<mesh::Mesh> mesh;
+  std::shared_ptr<mesh::MeshTags<std::int32_t>> ft;
+  std::shared_ptr<const common::IndexMap> index_map;
+  std::shared_ptr<fem::FunctionSpace> V;
+  std::shared_ptr<fem::Function<T>> u, u_n, v_n, g, dg, c0, rho0, delta0;
+  std::shared_ptr<fem::Form<T>> a, L;
+  std::shared_ptr<la::Vector<T>> m, b;
+
+  std::span<T> g_, dg_, m_, b_, out;
+  std::span<const T> _m, _b;
+
+  // Operators
+  std::shared_ptr<Stiffness2D<T, P, Q>> lin_op, att_op;
+
+  // Operators' coefficients
+  std::shared_ptr<fem::Function<T>> lin_coeff, att_coeff;
+  std::span<T> lin_coeff_, att_coeff_;
+};
+
+template <typename T>
+const T compute_diffusivity_of_sound(const T w0, const T c0, const T alpha){
+  const T diffusivity = 2*alpha*c0*c0*c0/w0/w0;
+
+  return diffusivity;
 }
