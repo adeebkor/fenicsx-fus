f0cc6b6d5e10d54d31078144849b8f4c7c158691
adeebkor <mabm4@cam.ac.uk>
2023-03-22 16:00:29 +0000

    Rename mesh folder

--- cpp/fenicsx-sf/benchmarks/HITU/W-H131-50W-WATER/main.cpp
+++ cpp/fenicsx-sf/benchmarks/HITU/W-H131-50W-WATER/main.cpp
@@ -54,7 +54,7 @@ int main(int argc, char* argv[])
     // Read mesh and mesh tags
     auto element = fem::CoordinateElement(mesh::CellType::hexahedron, 1);
     io::XDMFFile fmesh(MPI_COMM_WORLD,
-    "/home/mabm4/rds/hpc-work/mesh/S1/H131/mesh.xdmf", "r");
+    "/home/mabm4/rds/hpc-work/mesh/HITU/H131/mesh.xdmf", "r");
     auto mesh = std::make_shared<mesh::Mesh>(
       fmesh.read_mesh(element, mesh::GhostMode::none, "transducer_3d_2"));
     mesh->topology().create_connectivity(2, 3);
