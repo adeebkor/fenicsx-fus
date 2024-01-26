cfe344bd3923b4a4b2f444745babbc87be93d195
adeebkor <mabm4@cam.ac.uk>
2024-01-26 07:12:59 +0000

    Update code and rename folder

--- cpp/fenicsx-sf/experiments/measure_vector_assembly_speed/forms.py
+++ cpp/fenicsx-sf/experiments/measure_vector_assembly_speed/forms.py
@@ -22,6 +22,7 @@ V_DG = FunctionSpace(mesh, element_DG)
 c0 = Coefficient(V_DG)
 rho0 = Coefficient(V_DG)
 delta0 = Coefficient(V_DG)
+beta0 = Coefficient(V_DG)
 
 u = Coefficient(V)
 u_n = Coefficient(V)
@@ -37,9 +38,13 @@ md = {"quadrature_rule": "GLL", "quadrature_degree": qdegree[Q]}
 # Define forms
 a0 = inner(u/rho0/c0/c0, v) * dx(metadata=md)
 a1 = inner(delta0/rho0/c0/c0/c0*u, v) * ds(2, metadata=md)
+a2 = - inner(2*beta0/rho0/rho0/c0/c0/c0/c0*u_n*u, v) * dx(metadata=md)
 
+L0 = - inner(1/rho0*grad(u_n), grad(v)) * dx(metadata=md)
 L1 = inner(1/rho0*g, v) * ds(1, metadata=md)
 L2 = - inner(1/rho0/c0*v_n, v) * ds(2, metadata=md)
+L3 = - inner(delta0/rho0/c0/c0*grad(v_n), grad(v)) * dx(metadata=md)
 L4 = inner(delta0/rho0/c0/c0*dg, v) * ds(1, metadata=md)
+L5 = inner(2*beta0/rho0/rho0/c0/c0/c0/c0*v_n*v_n, v) * dx(metadata=md)
 
-forms = [a0, a1, L1, L2, L4]
+forms = [a0, a1, a2, L0, L1, L2, L3, L4, L5]
