c6b0b61e36dd29ad28c01b50b69b5dfa7422c1aa
Adeeb Arif Kor <mabm4@cam.ac.uk>
2024-01-27 13:27:06 +0800

    isort fenicsx-sf forms.py

--- cpp/fenicsx-sf/experiments/measure_fraction_of_peak_performance/forms.py
+++ cpp/fenicsx-sf/experiments/measure_fraction_of_peak_performance/forms.py
@@ -1,6 +1,6 @@
 import basix.ufl_wrapper
-from ufl import (VectorElement, Mesh, FunctionSpace, Coefficient, TestFunction,
-                 hexahedron, inner, grad, ds, dx)
+from ufl import (Coefficient, FunctionSpace, Mesh, TestFunction, VectorElement,
+                 ds, dx, grad, hexahedron, inner)
 
 P = 6  # Degree of polynomial basis
 Q = P + 1  # Number of quadrature points
